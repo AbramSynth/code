d5581d762f17f2e705b8d764c79add76e25e1454
AbramSynth <learn@repair69.com>
2024-02-19 11:50:55 -0700

    added x and y axis functionality

--- src/javascriptaio/README.md
+++ src/javascriptaio/README.md
@@ -7,12 +7,24 @@ A starter program for writing JavaScript games.
 
 1. include gamemlet.js in an html doc containing an element with an id of 'ball'.
 
-```html
+```
+html
 <div id="ball">@</div>
 <script src ="gamelet.js"></script>
 ```
 
-2. the script will detect when the left or right arrow keys are pressed and will move the ball element.
+2. the script will detect when the left, right, up, or down arrow keys are pressed and will move the ball element accordingly.
+
+
+3. adjust the value before px to increse length of movement. Default is no value (1px).
+
+
+```
+function refresh() {
+    ball.style.left = positionX + 'px';
+    ball.style.top = positionY + 'px';
+}
+```
 
 
 
---
+++ src/javascriptaio/gameletOG.js
@@ -0,0 +1,32 @@
+/*
+Instructions: include gamelet.js, 
+
+gamelet is an html doc containing element with id of ball. detects right and left keyboard movements for what i dont know.
+
+*/
+
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+
+
+/* responds to keypress by updating left and right position */
+function handleKeyPress(e) {
+    if (e.code === 'ArrowLeft') {
+        position = position - 10;
+    }
+    if (e.code === 'ArrowRight') {
+        position = position + 10;
+    } 
+    if (position < 0) {
+        position = 0;
+    }
+    refresh ();
+}
+
+/* refresh changes position of ball */
+
+function refresh() {
+    ball.style.left = position + 'px';
+}
+
